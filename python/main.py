062c5acab4d01a7c2871f009a547e24a4b3c6e0e
Aslıhan Özkan <114191738+aslihnzk@users.noreply.github.com>
2023-03-30 17:54:32 +0300

    Add files via upload

---
+++ python/main.py
@@ -0,0 +1,46 @@
+
+#Python Veri Tipleri
+# int
+sayi=2
+print(sayi)
+print(type(sayi))
+
+
+# float
+sayi1= 0.5
+print(sayi1)
+print(type(sayi1))
+
+# complex : Karışık sayıları ifade eder.
+a = 2e
+print(a)
+print(type(a))
+
+# str : metinsel türü ifade eder 
+baslik = "Veri Tipleri"
+print(baslik )
+print(type(baslik))
+ 
+ # bool : True-False sonuç döndürür.
+p = 5
+s = 55
+print(p>s)
+
+# tuple : () ile tanımlanan listeleme veri türüdür.
+b = ("Java","Python","C#")
+print(b[0],b[1],b[2])
+print(type(b)) 
+
+# list : Listeleme yapar.
+x = ["Ankara","İstanbul","Antalya"]
+print(x)
+print(type(x))
+
+#set : Kümedir ,listeden farkı ise birden fazla tekrar eden elemanlar içerir,küme benzersiz elemanlar içerir,sırasızdır.
+
+#Boş veri : None
+
+# dict : Haritalama veri türlerini ifade eder.
+c = {"name" : "clio","try" : 25}
+print(c)
+print(type(c))
